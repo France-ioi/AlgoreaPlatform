3a77ae6ad22e0cbb1cf77ee32e1c416875c95af8
Elie Roux <elie.roux@telecom-bretagne.eu>
2016-05-05 18:02:39 +0200

    minification-proof

--- algorea.js
+++ algorea.js
@@ -3,6 +3,6 @@
 // Make sure to include the `ui.router` module as a dependency
 var app = angular.module('algorea', ['ui.router', 'ui.bootstrap', 'franceIOILogin', 'ngSanitize','small-ui-confirm']);
 
-app.run(function($window) {
+app.run(['$window', function($window) {
 	$window.ga('create', 'UA-9457907-5', 'auto');
-})
+}]);
