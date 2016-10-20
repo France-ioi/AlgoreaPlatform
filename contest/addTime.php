e5f491e54ae605927bf26c16edf037b59a6500aa
Elie Roux <elie.roux@telecom-bretagne.eu>
2016-10-20 14:04:51 +0200

    handle group time addition

--- contest/addTime.php
+++ contest/addTime.php
@@ -2,8 +2,11 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Ajout de temps sur des concours</title>
+<title>Ajout de temps sur un concours</title>
 <link rel="stylesheet" href="/bower_components/bootstrap/dist/css/bootstrap.min.css">
+<style>
+body {width: 800px;margin-left:auto;margin-right:auto;margin-top:50px;}
+</style>
 </head>
 <body>
 <?php
@@ -39,12 +42,16 @@ require_once __DIR__.'/common.php';
 
 function syncDebug($type, $b_or_e, $subtype='') {}
 
-function addTimeToUser($idUser, $idGroupSelf, $idItem, $nbSeconds) {
+function addTimeToUser($idUser, $idGroupSelf, $idItem, $nbSeconds, $add) {
 	global $db;
-	$stmt = $db->prepare('select ID, sContestStartDate, sFinishDate, NOW() as now from users_items
+	$stmt = $db->prepare('select ID, sContestStartDate, sFinishDate, NOW() as now, TIME_TO_SEC(sAdditionalTime) as additionalSecs from users_items
 		where idUser = :idUser and idItem = :idItem;');
 	$stmt->execute(['idUser' => $idUser, 'idItem' => $idItem]);
 	$userItem = $stmt->fetch();
+	$totalAdditionalTime = $nbSeconds;
+	if ($add && $userItem) {
+		$totalAdditionalTime = $nbSeconds+$userItem['additionalSecs'];
+	}
 	if ($userItem && $userItem['sFinishDate']) {
 		// user has finished the contest, we must reopen it, under certain conditions:
 		$userFinishDate = new DateTime($userItem['sFinishDate']);
@@ -58,15 +65,15 @@ function addTimeToUser($idUser, $idGroupSelf, $idItem, $nbSeconds) {
 		}
 	}
 	// TODO: what if the user has a different user_item in its client?
-	$stmt = $db->prepare('insert into users_items (idUser, idItem, sAdditionalTime) values (:idUser, :idItem, SEC_TO_TIME(:nbSeconds)) on duplicate key update sAdditionalTime = SEC_TO_TIME(:nbSeconds), sFinishDate = NULL;');
-	$stmt->execute(['idItem' => $idItem, 'idUser' => $idUser, 'nbSeconds' => $nbSeconds]);
+	$stmt = $db->prepare('insert into users_items (idUser, idItem, sAdditionalTime) values (:idUser, :idItem, SEC_TO_TIME(:totalAdditionalTime)) on duplicate key update sAdditionalTime = SEC_TO_TIME(:totalAdditionalTime), sFinishDate = NULL;');
+	$stmt->execute(['idItem' => $idItem, 'idUser' => $idUser, 'totalAdditionalTime' => $totalAdditionalTime]);
 	if ($userItem && $userItem['sFinishDate']) {
 		$res = openContest($idItem, $idUser, $idGroupSelf, true);
 		if (!$res['success']) {
 			return $res;
 		}
 	}
-	return ['success' => true];
+	return ['success' => true, 'totalAdditionalTime' => $totalAdditionalTime];
 }
 
 function checkContestAccess($idItem, $contestList) {
@@ -81,12 +88,43 @@ function checkContestAccess($idItem, $contestList) {
 
 function getUserFromLogin($login) {
 	global $db;
-	$stmt = $db->prepare('select count(groups.ID) as ok, users.ID as idUser, users.idGroupSelf as idGroupSelf from groups 
+	$stmt = $db->prepare('select users.ID as idUser, users.idGroupSelf as idGroupSelf, users.sLogin from groups 
 		join groups_ancestors on groups_ancestors.idGroupChild = groups.ID
 		join users on users.idGroupSelf = groups_ancestors.idGroupChild
 		where groups_ancestors.idGroupAncestor = :idGroupOwned and users.sLogin = :login;');
-	$stmt->execute(['idGroupOwned' => $_SESSION['login']['idGroupOwned'], 'login' => $_GET['login']]);
+	$stmt->execute(['idGroupOwned' => $_SESSION['login']['idGroupOwned'], 'login' => $login]);
 	return $stmt->fetch();
+}
+
+function getUserListFromGroupName($groupName) {
+	global $db;
+	$stmt = $db->prepare('select groups.ID from groups
+		join groups_ancestors on groups_ancestors.idGroupChild = groups.ID
+		where groups_ancestors.idGroupAncestor = :idGroupOwned and groups.sName = :groupName;');
+	$stmt->execute(['idGroupOwned' => $_SESSION['login']['idGroupOwned'], 'groupName' => $groupName]);
+	$idGroup = $stmt->fetchColumn();
+	if (!$idGroup) {
+		return ['success' => false, 'error' => 'impossible de trouver l\'utilisateur ou le groupe '.$groupName];
+	}
+	$stmt = $db->prepare('select users.ID as idUser, users.idGroupSelf, users.sLogin from users
+		join groups_ancestors on groups_ancestors.idGroupChild = users.idGroupSelf
+		where groups_ancestors.idGroupAncestor = :idGroup;');
+	$stmt->execute(['idGroup' => $idGroup]);
+	$users = $stmt->fetchAll();
+	if (!$users || !count($users)) {
+	return ['success' => false, 'error' => 'le groupe '.$groupName.' ne contient aucun utilisateur'];	
+	}
+	return ['success' => true, 'usersList' => $users];
+}
+
+function getUsersListFromRequest($loginOrGroupName) {
+	$loginOrGroupName = trim($loginOrGroupName);
+	$user = getUserFromLogin($loginOrGroupName);
+	if (!$user) {
+		return getUserListFromGroupName($loginOrGroupName);
+	} else {
+		return ['success' => true, 'usersList' => [$user]];
+	}
 }
 
 function handleRequest($request) {
@@ -95,35 +133,51 @@ function handleRequest($request) {
 		echo '<p>Erreur : vous n\'avez pas accès au concours ID '.$request['idItem'].'</p>';
 		return;
 	}
-	$user = getUserFromLogin($request['login']);
-	if (!$user || !$user['ok'] || $user['ok'] == '0') {
-		echo 'Erreur: cet utilisateur n\'existe pas ou il n\'appartient pas à un de vos groupes.';
+	$add = (isset($request['type']) && $request['type'] == 'add');
+	$userData = getUsersListFromRequest($request['login']);
+	if (!$userData['success']) {
+		echo $userData['error'];
 		return;
 	}
+	$userList = $userData['usersList'];
 	$nbSeconds = intval($_GET['nbMinutes'] * 60);
-	$res = addTimeToUser($user['idUser'], $user['idGroupSelf'], $request['idItem'], $nbSeconds);
-	if (!$res['success']) {
-		echo 'Erreur: '.$res['error'];
-	} else {
-		echo "<p>L'utilisateur ".$request['login']." bénéficie désormais de ".$request['nbMinutes']." supplémentaires sur le concours ID ".$request['idItem']."</p>";
+	foreach($userList as $user) {
+		$res = addTimeToUser($user['idUser'], $user['idGroupSelf'], $request['idItem'], $nbSeconds, $add);
+		if (!$res['success']) {
+			echo 'Erreur: '.$res['error'];
+		} else {
+			$nbMinutes = intval($res['totalAdditionalTime']) / 60;
+			echo "<p>L'utilisateur ".$user['sLogin']." bénéficie désormais de ".$nbMinutes."mn supplémentaires sur le concours ID ".$request['idItem']."</p>";
+		}
 	}
 }
 
-if (isset($_GET['login']) && $_GET['login'] && isset($_GET['nbMinutes']) && $_GET['nbMinutes'] && isset($_GET['idItem']) && $_GET['idItem']) {
+if (isset($_GET['login']) && $_GET['login'] && isset($_GET['nbMinutes']) && isset($_GET['idItem']) && $_GET['idItem']) {
 	handleRequest($_GET);
 }
 
 ?>
+<h1>Ajout de temps sur un concours</h1>
+
 <p>Sur cette page vous pouvez ajouter du temps de concours à un élève qui est dans un de vos groupes :</p>
 
 <form method="get">
 	<div class="form-group">
-	  <label for="login">Login de l'utilisateur :</label>
+	  <label for="login">Login de l'utilisateur ou nom exact du groupe :</label>
 	  <input type="text" class="form-control" id="login" name="login">
 	</div>
 	<div class="form-group">
-	  <label for="nbMinutes">Nombre de minutes à rajouter :</label>
+	  <label for="nbMinutes">Nombre de minutes de temps supplémentaire (peut être négatif) :</label>
 	  <input type="text" class="form-control" id="nbMinutes" name="nbMinutes">
+	</div>
+	<div class="form-group">
+	  Assigner ce temps supplémentaire ou l'ajouter au temps supplémentaire déjà présent ?
+	  <div class="radio">
+	    <label><input type="radio" name="type" value="replace" checked>Assigner</label>
+	  </div>
+	  <div class="radio">
+	    <label><input type="radio" name="type" value="add">ajouter</label>
+	  </div>
 	</div>
 	<div class="form-group">
 		<label for="idItem">Nom du concours sur lequel le temps sera rajouté :</label>
