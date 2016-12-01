b5a0abe058bdf7aa62514a5cd96615f082f9cd3e
Elie Roux <elie.roux@telecom-bretagne.eu>
2016-12-01 14:18:37 +0100

    fix template prefix

--- admin/itemsCtrl.js
+++ admin/itemsCtrl.js
@@ -847,14 +847,14 @@ angular.module('algorea')
    }]);
 
 angular.module('algorea')
-   .controller('AccessModeCtrl', ['$scope', function($scope) {
+   .controller('AccessModeCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
       $scope.shouldBeOpen = false;
       $scope.data = {
          bAccessRestricted: true,
          bAlwaysVisible: 1
       };
       $scope.open = function() {
-         $scope.showGenDialog('tabs/accessEditDialog.html', 'items_items', 'item_item', $scope.$parent.item_item);
+         $scope.showGenDialog($rootScope.templatesPrefix+'/admin/tabs/accessEditDialog.html', 'items_items', 'item_item', $scope.$parent.item_item);
       };
 
 
