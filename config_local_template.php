2b1d34232c1f1c9007df99d2fe304498bdfd89cf
Michel Blockelet <blockelet@crans.org>
2017-08-26 15:14:21 +0100

    config template: add assetsBaseUrl, some explainations

--- config_local_template.php
+++ config_local_template.php
@@ -36,16 +36,31 @@ $config->shared->OrphanedRootItemId = "4004";
 // The IDs can be chosen arbitrarily, but must be distinct for the IDs of other
 // domains.
 
+// * Start of configuration block for one domain
+
 $config->shared->domains['default'] = (object) array();
+// Title shown on top left
 $config->shared->domains['default']->title = "Algorea Platform";
+// Platform default language
 $config->shared->domains['default']->defaultLanguage = 'fr';
 $config->shared->domains['default']->defaultAngularLocale = 'fr-fr';
+// Custom i18n strings to inclulde
 $config->shared->domains['default']->customStringsName = null;
+// Tagline for the platform
 $config->shared->domains['default']->taglineHtml = "";
+// Domain
 $config->shared->domains['default']->domain = "algorea.example.com";
+// Base URL of the platform
 $config->shared->domains['default']->baseUrl = "http://algorea.example.com";
+// Assets base URL, by default the same base URL of the platform, except if you
+// want to serve assets from another URL
+$config->shared->domains['default']->assetsBaseUrl = "http://algorea.example.com";
+// Intro animation
 $config->shared->domains['default']->animationHtmlFile = null;
+// Activate Forum
 $config->shared->domains['default']->usesForum = false;
+// Item IDs for the various base platform items; can be anything as long as
+// it's not already in use by another platform in the same database
 $config->shared->domains['default']->PlatformItemId = "4028";
 $config->shared->domains['default']->CustomProgressItemId = "4020";
 $config->shared->domains['default']->OfficialProgressItemId = "4021";
@@ -55,7 +70,14 @@ $config->shared->domains['default']->ContestRootItemId = "4002";
 $config->shared->domains['default']->CustomContestRootItemId = "4003";
 $config->shared->domains['default']->ProgressRootItemId = "4026";
 $config->shared->domains['default']->OfficialContestRootItemId = "4006";
+// Default path when we arrive on the platform
 $config->shared->domains['default']->defaultPath = "/contents/4026/4020";
+
+// Tabs on top of the UI
+// Each tab is an array with:
+//  * title: an i18next identifier
+//  * path: item IDs path in the platform
+//  * icon: the material icons identifier
 $config->shared->domains['default']->tabs = array();
 $config->shared->domains['default']->tabs[0] = array(
         'title' => 'menu_discover',
@@ -72,6 +94,8 @@ $config->shared->domains['default']->tabs[3] = array(
         'path' => '4002/4006',
         'icon' => 'create'
         );
+
+// * End of configuration block for one domain
 
 // *** Domain selection
 $thisDomain = 'default';
