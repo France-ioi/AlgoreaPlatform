France-ioi/AlgoreaPlatform

commit 3e79a7e7e5d38a98444fa049e853cb6ab48eadff

style: prevent mobile horizontal scroll when left sidebar is open

algorea.css
@@ -12,6 +12,9 @@ body {
    /*overflow-y: scroll;*/
    overflow-x: auto;
 }
+body.mobile-layout.sidebar-left-open {
+  overflow-x: hidden;
+}
 
 /*
  * Header colors

index.php
@@ -99,7 +99,7 @@
     }
     </style>
 </head>
-<body ng-controller="layoutController" id="body" ng-cloak ng-class="{'mobile-layout': isMobileLayout}">
+<body ng-controller="layoutController" id="body" ng-cloak ng-class="{'mobile-layout': isMobileLayout, 'sidebar-left-open': sidebarLeftIsOpen}">
 <?php if ($animationHtmlFile): ?>
   <iframe id="animation-debut" src="<?= $animationHtmlFile ?>" onclick="animationFinished()" style="display:none;"></iframe>
 <?php endif; ?>

commit 75368fad6c8186daef70f230cfdc21ff9f0a4621

fix layout big bug introduced in commit 3e79a7e7e

algorea.css
@@ -12,7 +12,7 @@ body {
    /*overflow-y: scroll;*/
    overflow-x: auto;
 }
-body.mobile-layout.sidebar-left-open {
+body.mobile-layout.has-sidebar-left-open {
   overflow-x: hidden;
 }
 

index.php
@@ -99,7 +99,7 @@
     }
     </style>
 </head>
-<body ng-controller="layoutController" id="body" ng-cloak ng-class="{'mobile-layout': isMobileLayout, 'sidebar-left-open': sidebarLeftIsOpen}">
+<body ng-controller="layoutController" id="body" ng-cloak ng-class="{'mobile-layout': isMobileLayout, 'has-sidebar-left-open': sidebarLeftIsOpen}">
 <?php if ($animationHtmlFile): ?>
   <iframe id="animation-debut" src="<?= $animationHtmlFile ?>" onclick="animationFinished()" style="display:none;"></iframe>
 <?php endif; ?>