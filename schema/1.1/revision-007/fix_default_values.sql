ecd9afdf63e94db2a7541d5021c134d21169613a
mobydimk <mobydimk@gmail.com>
2017-11-17 19:27:08 +0400

    migrations bugfix

--- schema/1.1/revision-007/fix_default_values.sql
+++ schema/1.1/revision-007/fix_default_values.sql
@@ -1,41 +1,41 @@
-ALTER TABLE `users` CHANGE `sSalt` `sSalt` VARCHAR(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
-ALTER TABLE `users` CHANGE `sRecover` `sRecover` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
-ALTER TABLE `users` CHANGE `sTimeZone` `sTimeZone` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
-ALTER TABLE `users` CHANGE `sAddress` `sAddress` MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'User\'s address';
-ALTER TABLE `users` CHANGE `sZipcode` `sZipcode` LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'User\'s postal code';
-ALTER TABLE `users` CHANGE `sCity` `sCity` LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'User\'s city';
-ALTER TABLE `users` CHANGE `sLandLineNumber` `sLandLineNumber` LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'User\'s phone number';
-ALTER TABLE `users` CHANGE `sCellPhoneNumber` `sCellPhoneNumber` LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'User\'s mobil phone number';
-ALTER TABLE `users` CHANGE `bPublicFirstName` `bPublicFirstName` TINYINT(4) NOT NULL DEFAULT '0' COMMENT 'Publicly show user\'s first name';
-ALTER TABLE `users` CHANGE `bPublicLastName` `bPublicLastName` TINYINT(4) NOT NULL DEFAULT '0' COMMENT 'Publicly show user\'s first name';
-ALTER TABLE `users` CHANGE `sFreeText` `sFreeText` MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
-ALTER TABLE `users` CHANGE `sWebSite` `sWebSite` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
-ALTER TABLE `users` CHANGE `sLastIP` `sLastIP` VARCHAR(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
-ALTER TABLE `users` CHANGE `iStepLevelInSite` `iStepLevelInSite` INT(11) NOT NULL DEFAULT '0' COMMENT 'User''s level';
-ALTER TABLE `users` CHANGE `nbHelpGiven` `nbHelpGiven` INT(11) NOT NULL DEFAULT '0' COMMENT 'TODO';
-ALTER TABLE `users` CHANGE `idGroupSelf` `idGroupSelf` BIGINT(20) NULL DEFAULT NULL;
-ALTER TABLE `users` CHANGE `idGroupAccess` `idGroupAccess` BIGINT(20) NULL DEFAULT NULL;
-ALTER TABLE `users` CHANGE `sLastLoginDate` `sLastLoginDate` DATETIME NULL DEFAULT NULL;
+ALTER TABLE `users` CHANGE `sSalt` `sSalt` VARCHAR(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+CHANGE `sRecover` `sRecover` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+CHANGE `sTimeZone` `sTimeZone` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+CHANGE `sAddress` `sAddress` MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'User\'s address',
+CHANGE `sZipcode` `sZipcode` LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'User\'s postal code',
+CHANGE `sCity` `sCity` LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'User\'s city',
+CHANGE `sLandLineNumber` `sLandLineNumber` LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'User\'s phone number',
+CHANGE `sCellPhoneNumber` `sCellPhoneNumber` LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'User\'s mobil phone number',
+CHANGE `bPublicFirstName` `bPublicFirstName` TINYINT(4) NOT NULL DEFAULT '0' COMMENT 'Publicly show user\'s first name',
+CHANGE `bPublicLastName` `bPublicLastName` TINYINT(4) NOT NULL DEFAULT '0' COMMENT 'Publicly show user\'s first name',
+CHANGE `sFreeText` `sFreeText` MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+CHANGE `sWebSite` `sWebSite` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+CHANGE `sLastIP` `sLastIP` VARCHAR(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+CHANGE `iStepLevelInSite` `iStepLevelInSite` INT(11) NOT NULL DEFAULT '0' COMMENT 'User''s level',
+CHANGE `nbHelpGiven` `nbHelpGiven` INT(11) NOT NULL DEFAULT '0' COMMENT 'TODO',
+CHANGE `idGroupSelf` `idGroupSelf` BIGINT(20) NULL DEFAULT NULL,
+CHANGE `idGroupAccess` `idGroupAccess` BIGINT(20) NULL DEFAULT NULL,
+CHANGE `sLastLoginDate` `sLastLoginDate` DATETIME NULL DEFAULT NULL;
 
 
-ALTER TABLE `history_users` CHANGE `sSalt` `sSalt` VARCHAR(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
-ALTER TABLE `history_users` CHANGE `sRecover` `sRecover` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
-ALTER TABLE `history_users` CHANGE `sTimeZone` `sTimeZone` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
-ALTER TABLE `history_users` CHANGE `sAddress` `sAddress` MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'User\'s address';
-ALTER TABLE `history_users` CHANGE `sZipcode` `sZipcode` LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'User\'s postal code';
-ALTER TABLE `history_users` CHANGE `sCity` `sCity` LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'User\'s city';
-ALTER TABLE `history_users` CHANGE `sLandLineNumber` `sLandLineNumber` LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'User\'s phone number';
-ALTER TABLE `history_users` CHANGE `sCellPhoneNumber` `sCellPhoneNumber` LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'User\'s mobil phone number';
-ALTER TABLE `history_users` CHANGE `bPublicFirstName` `bPublicFirstName` TINYINT(4) NOT NULL DEFAULT '0' COMMENT 'Publicly show user\'s first name';
-ALTER TABLE `history_users` CHANGE `bPublicLastName` `bPublicLastName` TINYINT(4) NOT NULL DEFAULT '0' COMMENT 'Publicly show user\'s first name';
-ALTER TABLE `history_users` CHANGE `sFreeText` `sFreeText` MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
-ALTER TABLE `history_users` CHANGE `sWebSite` `sWebSite` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
-ALTER TABLE `history_users` CHANGE `sLastIP` `sLastIP` VARCHAR(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
-ALTER TABLE `history_users` CHANGE `iStepLevelInSite` `iStepLevelInSite` INT(11) NOT NULL DEFAULT '0' COMMENT 'User''s level';
-ALTER TABLE `history_users` CHANGE `nbHelpGiven` `nbHelpGiven` INT(11) NOT NULL DEFAULT '0' COMMENT 'TODO';
-ALTER TABLE `history_users` CHANGE `idGroupSelf` `idGroupSelf` BIGINT(20) NULL DEFAULT NULL;
-ALTER TABLE `history_users` CHANGE `idGroupAccess` `idGroupAccess` BIGINT(20) NULL DEFAULT NULL;
-ALTER TABLE `history_users` CHANGE `sLastLoginDate` `sLastLoginDate` DATETIME NULL DEFAULT NULL;
+ALTER TABLE `history_users` CHANGE `sSalt` `sSalt` VARCHAR(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+CHANGE `sRecover` `sRecover` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+CHANGE `sTimeZone` `sTimeZone` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+CHANGE `sAddress` `sAddress` MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'User\'s address',
+CHANGE `sZipcode` `sZipcode` LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'User\'s postal code',
+CHANGE `sCity` `sCity` LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'User\'s city',
+CHANGE `sLandLineNumber` `sLandLineNumber` LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'User\'s phone number',
+CHANGE `sCellPhoneNumber` `sCellPhoneNumber` LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'User\'s mobil phone number',
+CHANGE `bPublicFirstName` `bPublicFirstName` TINYINT(4) NOT NULL DEFAULT '0' COMMENT 'Publicly show user\'s first name',
+CHANGE `bPublicLastName` `bPublicLastName` TINYINT(4) NOT NULL DEFAULT '0' COMMENT 'Publicly show user\'s first name',
+CHANGE `sFreeText` `sFreeText` MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+CHANGE `sWebSite` `sWebSite` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+CHANGE `sLastIP` `sLastIP` VARCHAR(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+CHANGE `iStepLevelInSite` `iStepLevelInSite` INT(11) NOT NULL DEFAULT '0' COMMENT 'User''s level',
+CHANGE `nbHelpGiven` `nbHelpGiven` INT(11) NOT NULL DEFAULT '0' COMMENT 'TODO',
+CHANGE `idGroupSelf` `idGroupSelf` BIGINT(20) NULL DEFAULT NULL,
+CHANGE `idGroupAccess` `idGroupAccess` BIGINT(20) NULL DEFAULT NULL,
+CHANGE `sLastLoginDate` `sLastLoginDate` DATETIME NULL DEFAULT NULL;
 
 ALTER TABLE `history_users_items` CHANGE `nbTasksSolved` `nbTasksSolved` INT(11) NOT NULL DEFAULT '0';
 
