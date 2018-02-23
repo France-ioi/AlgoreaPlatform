b55f34ad783d69d00a8474b65463b2c091f8c078
Michel Blockelet <blockelet@crans.org>
2018-02-23 01:24:56 +0100

    syncRequests: fix groupsItemsAncestors filter

--- syncRequests/groupsItemsAncestors.php
+++ syncRequests/groupsItemsAncestors.php
@@ -5,7 +5,7 @@ class groupsItemsAncestors {
       $request = syncGetTablesRequests(array('groups_items' => true), false);
       $request = $request['groups_items'];
       $request["model"]["fields"]["idGroup"]["groupBy"] = "`groups_items`.`ID`";
-      $requests["groups_items"]["filters"]["ancestorsRead"] = array(
+      $request["filters"]["ancestorsRead"] = array(
          'modes' => array('select' => true), 
          "values" => array(
             "idGroupSelf" => $_SESSION['login']['idGroupSelf']
